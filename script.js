a23dba0564db7abd9a0d34104f0c1854f7984585
LucasAlves2803 <lucascosta2822@gmail.com>
2021-12-20 15:59:28 -0300

    novo_repostorio

---
+++ script.js
@@ -0,0 +1,8 @@
+// A função alerta irá mostrar uma caixinha de mensagem ao abrir o navegador
+//alert("Olá 2")
+
+// O console é um objeto, e o log() é apenas um método deste objeto.
+// Normalmente utilizamos o console.log 
+// para acompanhar os valores de variáveis durante a execução do código
+// Para identificar algum bug ou entender como o código está se comportando
+console.log("Testando console")
