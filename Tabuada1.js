368dc4241002b0daf539aab6966dd5d474f760da
Hugox96 <alveshugo2906@gmail.com>
2022-01-22 23:02:09 -0300

    novo arquivo

---
+++ Tabuada1.js
@@ -0,0 +1,11 @@
+// Para treinar as operações aritméticas básicas, vou criar uma tabuada simples.
+
+// Para isso vou criar uma função que realize um for percorrendo de 0 a 10 com um número de minha escolha
+function tabuada() {
+    var n = 5;
+    for(var i = 0; i<=15; i++){
+        console.log(`${n} X ${i} = ${n*i}`);
+    }
+}
+
+tabuada();
