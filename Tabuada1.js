4f0dbe418e5b2f40ae6768a2cf87e6435710bf12
Hugox96 <alveshugo2906@gmail.com>
2022-01-22 23:03:54 -0300

    novo arquivo

--- Tabuada1.js
+++ Tabuada1.js
@@ -2,9 +2,11 @@
 
 // Para isso vou criar uma função que realize um for percorrendo de 0 a 10 com um número de minha escolha
 function tabuada() {
-    var n = 5;
+    var n = 5; // Declarando a variável
+    
+    //criando um loop com for para ler de 0 a 15
     for(var i = 0; i<=15; i++){
-        console.log(`${n} X ${i} = ${n*i}`);
+        console.log(`${n} X ${i} = ${n*i}`); // Imprimindo o valor na tela
     }
 }
 
