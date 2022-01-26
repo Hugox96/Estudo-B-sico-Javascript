b7f82557fcfc610aeaccf9514cecc68490c9fdf3
Hugox96 <alveshugo2906@gmail.com>
2022-01-26 16:02:21 -0300

    Estudando sobre interacao com usuario

---
+++ ScriptTabuada2.js
@@ -0,0 +1,5 @@
+
+function tabuada(){
+    for(var count=1; count<=10 ; count++)
+     console.log("3 x "+count+" = " + (3*count) + "<br />");
+  }
--- Tabuada1.js
+++ Tabuada1.js
@@ -1,5 +1,7 @@
 // Para treinar as operações aritméticas básicas, vou criar uma tabuada simples.
 
+//https://www.javascriptprogressivo.net/2018/12/Como-Criar-Tabuada-JavaScript.html
+
 // Para isso vou criar uma função que realize um for percorrendo de 0 a 10 com um número de minha escolha
 function tabuada() {
     var n = 5; // Declarando a variável
