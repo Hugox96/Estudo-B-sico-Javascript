3aea18eff93a7e7163caef9bc36f034fbfe2627d
Hugox96 <alveshugo2906@gmail.com>
2022-02-16 20:40:59 -0300

    If e Else

---
+++ Aulas/If_and_Else.js
@@ -0,0 +1,16 @@
+// O If é usado para excutar um bloco quando uma condição é verdadeira
+
+if(condição = true) {
+    // O bloco será executado
+}
+
+// O Else é executado quando a condição do if não é verdadeira, em blocos distintos
+//Neste caso é uma condição simples, verdadeiro ou falso. Sim ou não.
+
+if(condição = false) {
+    // O bloco não sera executado
+}
+
+else{
+     //O bloco será executado
+}
