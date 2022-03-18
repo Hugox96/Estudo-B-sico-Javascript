514b6000d48003d48aa1499d33d732f4acf2e111
Hugox96 <alveshugo2906@gmail.com>
2022-03-18 20:42:00 -0300

    logicaIr

---
+++ Exercicios/Condicionais/ImpostoRenda.js
@@ -0,0 +1,16 @@
+let salario = 3000
+
+if(salario <= 1903){
+  console.log(" É isento de imposto de renda")
+}
+
+else if (1903 <= salario && salario < 2826)
+{
+  console.log("É descontado 7,5%")
+}
+
+else if (2826 <= salario && salario <= 3751)
+{
+  console.log("É descontado 15%")
+}
+
