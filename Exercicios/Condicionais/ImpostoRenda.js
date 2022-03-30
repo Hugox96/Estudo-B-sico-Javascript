e74e308d0c55356f3ac5a30156c53c33f55d07cb
Hugox96 <alveshugo2906@gmail.com>
2022-03-30 11:14:02 -0300

    Melhorando os codigos de Ir e votos

--- Exercicios/Condicionais/ImpostoRenda.js
+++ Exercicios/Condicionais/ImpostoRenda.js
@@ -1,18 +1,22 @@
 let salario = 3000
 
-if (salario <= 1903) {
+
+
+if (salario < 1903) {
   console.log(" É isento de imposto de renda")
 }
 
 
 else {
   if (1903 <= salario && salario < 2826) {
-    console.log("É descontado 7,5%")
+    var faixa1 = salario*3 
+    console.log(`É descontado 7,5%, ${faixa1}`)
   }
 
   else {
     if (2826 <= salario && salario <= 3751) {
-      console.log("É descontado 15%")
+      var faixa2 = (0.15 * salario) - 354.80
+      console.log(`É descontado 15%, ${faixa2}`)
     }
 
     else {
@@ -26,4 +30,3 @@ else {
     }
   }
 }
-
