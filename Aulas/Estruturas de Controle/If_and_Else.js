9c7c813323d8cfa6bc44c2d18649c31515533cb8
Hugox96 <alveshugo2906@gmail.com>
2022-05-20 20:06:49 -0300

    Organizando os estudos

--- Aulas/Estruturas de Controle/If_and_Else.js
+++ Aulas/Estruturas de Controle/If_and_Else.js
@@ -5,8 +5,8 @@ if(condição = true) {
 }
 
 // O Else é executado quando a condição do if não é verdadeira, em blocos distintos
-//Neste caso é uma condição simples, verdadeiro ou falso. Sim ou não.
 
+//Neste caso é uma condição simples, verdadeiro ou falso. Sim ou não.
 if(condição = false) {
     // O bloco não sera executado
 }
