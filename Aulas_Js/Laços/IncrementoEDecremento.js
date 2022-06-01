927cc29c6cb87249019ee10f81c11e425da65114
Hugox96 <alveshugo2906@gmail.com>
2022-06-01 20:52:40 -0300

    Estudando while e incrementos

---
+++ Aulas_Js/Laços/IncrementoEDecremento.js
@@ -0,0 +1,31 @@
+// Operadores de incremento
+
+// Online Javascript Editor for free
+
+// Operadores de incrementos e decrementos são muito utilizados em loops, e também são bem importantes para não criar um loop infinito
+
+// Vamos criar um loop com whíle 
+let Dolar = 5 
+let Real = 1
+
+//Enquanto Real for menor ou igual a dolar, o código rodará
+while (Real <= Dolar)
+{
+    console.log(`Real = ${Real}`)
+    Real ++ // Real vai de 1 até 5
+}
+
+
+// Vamos criar uma contagem regressiva agora na virado do ano
+let segundos = 10
+
+while(segundos>=1){
+    console.log(`${segundos}`)
+    if(segundos == 1) {
+    console.log("Feliz ano novo")
+    }
+    segundos --
+}
+
+
+
