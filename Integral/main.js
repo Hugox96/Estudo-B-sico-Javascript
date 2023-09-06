40317b9c0c97025b4c7c0afddf65ae22c2260705
Hugox96 <alveshugo2906@gmail.com>
2023-09-06 20:43:38 -0300

    ok

--- Integral/main.js
+++ Integral/main.js
@@ -5,17 +5,20 @@ document.addEventListener('DOMContentLoaded', function() {
         var expressionInput = document.getElementById('expression');
         var resultDiv = document.getElementById('result');
         var expression = expressionInput.value;
-        console.log(expression)
+        console.log("oi", expression)
 
         // Resto do seu código de cálculo de integral aqui...
          // Tente calcular a integral
          try {
+            console.log("estou no try")
             const nerdamer = require('nerdamer/all');
-            var integral = nerdamer.integrate(expression, 'x');
+            var integral = nerdamer.solverEquations(expression, 'x');
+            console.log(integral.toString())
 
             // Exiba o resultado como uma string
             resultDiv.textContent = 'Resultado: ' + integral.toString();
         } catch (error) {
+            console.log("estou no catch")
             resultDiv.textContent = 'Não foi possível calcular a integral.';
         }
     });
