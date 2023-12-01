9a1de2d94006b493248175011b22f71c62ff8583
Hugox96 <alveshugo2906@gmail.com>
2023-12-01 09:37:39 -0300

    ok

--- Exercicios_Js/Desafio3(MDC).js
+++ Exercicios_Js/Desafio3(MDC).js
@@ -15,3 +15,9 @@ function mcd(a, b) {
   var resultado = mcd(48, 18);
   console.log(resultado);
   
+
+  
+// Excelente exercicio!. Nesse caso ele começa com b igual 18 e o loop do while continua até que b seja diferente de 0. Ele começa com temp = 6, que neste caso é 18. Depois b se torna a%b que na primeira interação é 18.
+// Logo b será:
+// 1º loop 18, 2º loop 12, 3º loop 6, 4º loop 0(fim). 
+// Já a será o valor de b definido antes da operação do modulo, sendo 1º 18, 2º 12 e 3º 6(nosso valor).
