333d544b54d5e19b7f9301c73e5433ac467e5fb3
Hugox96 <alveshugo2906@gmail.com>
2024-01-03 19:44:23 -0300

    revisando

--- Aulas_Js/Objetos/objetoRevisao.js
+++ Aulas_Js/Objetos/objetoRevisao.js
@@ -8,37 +8,37 @@ console.log(produto)
 delete produto.preco
 delete produto["marca do produto"]
 
-console.log(produto)
+// console.log(produto)
 
 const carro = { 
-    modelo: 'A4',
-    valor: 89000,
+        modelo: 'A4',
+        valor: 89000,
     
     //criando um Objeto dentro do objeto carro
     proprietario:{
-    nome: 'Raul',
-    idade: 56,
+        nome: 'Raul',
+        idade: 56,
     //criando um Objeto dentro do objeto propretário
-    endereco: 
-            {
+    endereco: {
             logradouro: 'Rua ABC',
             numero: 123
-            }
+        }
     },
         condutores: [{
-        nome: 'Junior',
-        idade: 19
-        }, {
-           nome: 'Ana',
-           idade: 25
+            nome: 'Junior',
+            idade: 19
+                    }, 
+           {
+            nome: 'Ana',
+            idade: 25
            }],
            
     calcularValorSeguro: function()
     {
-         //....
+         
     }
 }
 
 carro.proprietario.endereco.numero = 1000
 carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
-console.log(carro)
+// console.log(carro)
