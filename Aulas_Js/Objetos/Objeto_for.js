f9650c142d4b7404acec4edef5fac889590c6095
Hugox96 <alveshugo2906@gmail.com>
2024-02-06 15:39:52 -0300

    revisando

--- Aulas_Js/Objetos/Objeto_for.js
+++ Aulas_Js/Objetos/Objeto_for.js
@@ -6,9 +6,11 @@ produto.preco = 220;
 
 console.log("1º print",produto)
 
-// Criando propriedades dinamicamente usando um loop
-for (let i = 1; i <= 3; i++) {
-    produto[`caracteristica${i}`] = `Valor ${i}`;
-}
+// // Criando propriedades dinamicamente usando um loop
+// for (let i = 1; i <= 3; i++) {
+//     produto[`caracteristica${i}`] = `Valor ${i}`;
+// }
+
+
 
 console.log(produto);
