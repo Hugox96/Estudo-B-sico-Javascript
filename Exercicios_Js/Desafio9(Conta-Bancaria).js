2b1b3c4c144301667cfb1abcd18a0f0d17b13aa7
Hugox96 <alveshugo2906@gmail.com>
2024-02-07 15:30:38 -0300

    ok

--- Exercicios_Js/Desafio9(Conta-Bancaria).js
+++ Exercicios_Js/Desafio9(Conta-Bancaria).js
@@ -12,19 +12,24 @@ sacar(valor){
     if (valor <= this.saldo){
         this.saldo -= valor;
         console.log(`Saque de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
+
+        if (this.saldo <= 0) {
+            console.log(`conta zerada ou negativa`);
+        }
     }
+
     else {console.log(`Saldo insuficiente para realizar o saque de ${valor}. Saldo atual: ${this.saldo}`);
+
 }}} 
 
 const salario = new ContaBancaria(2000)
 console.log(salario)
 
-// depositando
+depositando
 salario.depositar(100)
 
-for (let i = 0; i <= 5; i++){
-    salario.depositar(i)
-}
+//sacando
+salario.sacar(2115)
 
 
 
