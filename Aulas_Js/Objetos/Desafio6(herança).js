339e349c29bb00a76841af82990f4684945a4859
Hugox96 <alveshugo2906@gmail.com>
2024-02-06 22:48:33 -0300

    ok

---
+++ Aulas_Js/Objetos/Desafio6(herança).js
@@ -0,0 +1,24 @@
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} faz algum som.`);
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, breed) {
+      super(name);
+      this.breed = breed;
+    }
+  
+    speak() {
+      console.log(`${this.name} late.`);
+    }
+  }
+  
+  const dog = new Dog('Rex', 'Labrador');
+  dog.speak();
+  
