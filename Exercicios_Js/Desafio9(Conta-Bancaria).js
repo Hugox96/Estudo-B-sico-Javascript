0a593dc7745761e9cf77c1dcb661d0f962b678f7
Hugox96 <alveshugo2906@gmail.com>
2024-02-07 15:11:05 -0300

    ok

--- Exercicios_Js/Desafio9(Conta-Bancaria).js
+++ Exercicios_Js/Desafio9(Conta-Bancaria).js
@@ -2,5 +2,17 @@ class ContaBancaria {
     constructor(saldoInicial){
         this.saldo = saldoInicial
     }
+
+depositar(valor){
+    this.saldo += valor;
+    console.log(`Depósito de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
 }
+
+sacar(valor){
+    if (valor <= this.saldo){
+        this.saldo -= valor;
+        console.log(`Saque de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
+    }
+    else {console.log(`Saldo insuficiente para realizar o saque de ${valor}. Saldo atual: ${this.saldo}`);
+}}} 
 
