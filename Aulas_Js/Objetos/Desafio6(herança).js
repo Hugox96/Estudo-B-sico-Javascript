dcea8ae5a1d23db75ef135c6df5b01cedcd9bdb5
Hugox96 <alveshugo2906@gmail.com>
2024-02-07 10:54:12 -0300

    ok

--- Aulas_Js/Objetos/Desafio6(herança).js
+++ Aulas_Js/Objetos/Desafio6(herança).js
@@ -1,27 +1,33 @@
+// cria a classe pai animal
 class Animal {
     constructor(name) {
       this.name = name;
     }
-  
+  // cria o metodo da classe principal
     speak() {
       console.log(`${this.name} faz algum som.`);
     }
   }
   
+  // Cria uma herança da classe principal, sendo a classe cachorro
   class Dog extends Animal {
     constructor(name, breed) {
+      // herda o atributo nome da classe animal
       super(name);
+      // cria um novo atributo
       this.breed = breed;
     }
   
+    // cria um metodo da classe dog
     speak() {
       console.log(`${this.name} late.`);
     }
   }
   
+  // instancia a classe dog
   const dog = new Dog('Rex', 'Labrador');
   dog.speak();
-
+// instancia a classe animal
   const animal = new Animal("hugo")
   animal.speak()
   
