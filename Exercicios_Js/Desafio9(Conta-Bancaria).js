7d33b85460334a7ae94893ef3db9003f4abc49c2
Hugox96 <alveshugo2906@gmail.com>
2024-02-07 15:53:27 -0300

    ok

--- Exercicios_Js/Desafio9(Conta-Bancaria).js
+++ Exercicios_Js/Desafio9(Conta-Bancaria).js
@@ -1,35 +1,36 @@
 class ContaBancaria {
-    constructor(saldoInicial){
-        this.saldo = saldoInicial
+    constructor(saldoInicial) {
+      this.saldo = saldoInicial;
     }
-
-depositar(valor){
-    this.saldo += valor;
-    console.log(`Depósito de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
-}
-
-sacar(valor){
-    if (valor <= this.saldo){
+  
+    depositar(valor) {
+      this.saldo += valor;
+      console.log(`Depósito de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
+    }
+  
+    sacar(valor) {
+      if (valor <= this.saldo) {
         this.saldo -= valor;
         console.log(`Saque de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
-
-        if (this.saldo <= 0) {
-            console.log(`conta zerada ou negativa`);
-        }
+      } else {
+        console.log(`Saldo insuficiente para realizar o saque de ${valor}. Saldo atual: ${this.saldo}`);
+      }
     }
+  
+    transferir(destino, valor) {
+      if (valor <= this.saldo) {
+        this.sacar(valor);
+        destino.depositar(valor);
+        console.log(`Transferência de ${valor} realizada com sucesso para a conta de destino.`);
+      } else {
+        console.log(`Saldo insuficiente para realizar a transferência de ${valor}. Saldo atual: ${this.saldo}`);
+      }
+    }
+}
 
-    else {console.log(`Saldo insuficiente para realizar o saque de ${valor}. Saldo atual: ${this.saldo}`);
+const salario = new ContaBancaria(2000);
+const destino = new ContaBancaria(0); // Criando uma conta de destino
 
-}}} 
-
-const salario = new ContaBancaria(2000)
-console.log(salario)
-
-depositando
-salario.depositar(100)
-
-//sacando
-salario.sacar(2115)
-
-
-
+//depositando
+salario.depositar(10);
+salario.transferir(destino, 20); // Passando a conta de destino como argumento
