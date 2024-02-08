3a95898b544027d66aa7b2636b69c9d84e7a478a
Hugox96 <alveshugo2906@gmail.com>
2024-02-08 15:09:09 -0300

    ok

--- Aulas_Js/Desafio10(Gerenciador de tarefas).js
+++ Aulas_Js/Desafio10(Gerenciador de tarefas).js
@@ -2,8 +2,36 @@ class Gerenciador_Tarefas {
     constructor(){
     this.tarefas =[]
     }
-}
 
 adicionarTarefas(tarefa){
     this.tarefas.push(tarefa)
 }
+
+listarTarefas(){
+    if(this.tarefas.lenght ===0){
+        console.log("A lista de tarefas está vazia!")
+    }
+    else{
+        console.log("Listando tarefas")
+         this.tarefas.forEach((tarefa, indice) => {
+            console.log(`${indice + 1}. ${tarefa}`);
+        });
+    }
+}
+
+removertarefa(indice){
+    if(indice >=0 && indice < this.tarefas.lenght){
+        const tarefaRemovida = this.tarefas.splice(indice, 1);
+        console.log(`A tarefa "${tarefaRemovida}" foi removida com sucesso.`);
+    }
+    else {
+        console.log("Índice inválido. Nenhuma tarefa foi removida.");
+    }
+  }
+}
+
+const objetivos = new Gerenciador_Tarefas()
+objetivos.adicionarTarefas("Estudar JavaScript");
+objetivos.adicionarTarefas("Fazer compras");
+objetivos.listarTarefas()
+objetivos.removertarefa(0)
