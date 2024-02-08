713192613470a91a6e87c1a9367e1c07f01c9c60
Hugox96 <alveshugo2906@gmail.com>
2024-02-08 15:23:59 -0300

    ok

--- Exercicios_Js/Desafio9(Conta-Bancaria).js
+++ Exercicios_Js/Desafio9(Conta-Bancaria).js
@@ -1,6 +1,7 @@
 // Class principal ContaBancaria com a nossa variavel principal saldoInicial
 class ContaBancaria {
     constructor(saldoInicial) {
+      // saldo é a variavel mais importante, a partir dela que vamos fazer a operação
       this.saldo = saldoInicial;
     }
   
@@ -9,16 +10,18 @@ class ContaBancaria {
       this.saldo += valor;
       console.log(`Depósito de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
     }
-  
-    sacar(valor) {
+    //metodo para sacar o valor, nesse caso o valor é decrementado, já que estamos retirando parte do valor
+    sacar(valor, msg) {
       if (valor <= this.saldo) {
         this.saldo -= valor;
-        console.log(`Saque de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`);
+        msg = "saquei para comprar manga"
+        console.log(`Saque de ${valor} realizado com sucesso: ${msg}`);
+        console.log(`Novo saldo: ${this.saldo}`)
       } else {
         console.log(`Saldo insuficiente para realizar o saque de ${valor}. Saldo atual: ${this.saldo}`);
       }
     }
-  
+   // metodo para transferir o valor, nesse caso a logica de decrementador é mantida.
     transferir(destino, valor) {
       if (valor <= this.saldo) {
         this.sacar(valor);
@@ -30,9 +33,11 @@ class ContaBancaria {
     }
 }
 
-const salario = new ContaBancaria(2000);
-const destino = new ContaBancaria(0); // Criando uma conta de destino
+// instanciando a classe contabancaria
+const salario = new ContaBancaria(3000);
+const destino = new ContaBancaria(1); // Criando uma conta de destino
 
-//depositando
+// //depositando
 salario.depositar(10);
-salario.transferir(destino, 20); // Passando a conta de destino como argumento
+salario.sacar(20)
+// salario.transferir(destino, 20); // Passando a conta de destino como argumento
