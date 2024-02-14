9b5585e73cbdde2308f1c193ea03829fc10e08cc
Hugox96 <alveshugo2906@gmail.com>
2024-02-14 16:28:48 -0300

    ok

--- Exercicios_Js/Desafio11.(Jogo_Simples).js
+++ Exercicios_Js/Desafio11.(Jogo_Simples).js
@@ -1,11 +1,11 @@
 
 // classe principal com os atributos dos personagens
-class personagem {
-    constructor(nome, vida){
-    this.nome = nome;
+class Personagem {
+    constructor(nome, vida) {
+    this.nome = nome; 
     this.vida = vida;
  }
-}
+
 
 //Criando os metodos
 
@@ -16,18 +16,30 @@ receberDano(dano) {
 
     if(this.vida <= 30) {
         // irei criar daqui a pouco
-        this.UserBuster();
+        this.usarBuster();
     }
 
     if(this.vida == 0) {
         console.log("O personagem foi de F");
     }
+}    
 
 
-userBurst() {
+usarBurst() {
     console.log(`${this.nome} usou Burst e recuperou 50 pontos de vida.`);
     this.vida += 50;
     console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
+  }
+
+// usarBurst() {
+//     console.log(`${this.nome} usou Burst e recuperou 50 pontos de vida.`);
+//     this.vida += 50;
+//     console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
+//     }
 }
 
-}
+const jogador1 = new Personagem('Guerreiro', 100);
+const jogador2 = new Personagem('Mago', 80);
+
+jogador1.receberDano(30)
+jogador1.receberDano(40)
