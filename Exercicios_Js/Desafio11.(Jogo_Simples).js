fabf29c881cce5366a99dce3d337a7f6386a2c23
Hugox96 <alveshugo2906@gmail.com>
2024-02-14 16:17:26 -0300

    ok

--- Exercicios_Js/Desafio11.(Jogo_Simples).js
+++ Exercicios_Js/Desafio11.(Jogo_Simples).js
@@ -3,5 +3,19 @@
 class personagem {
     constructor(nome, vida){
     this.nome = nome;
-    this.vida = vida;}
+    this.vida = vida;
+ }
+}
+
+//Criando os metodos
+
+receberDano(dano) {
+    console.log(`${this.nome} recebeu ${dano} de dano.`);
+    this.vida -= dano;
+    console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
+
+    if(this.vida <= 30) {
+        // irei criar daqui a pouco
+        this.UserBuster()
+    }
 }
