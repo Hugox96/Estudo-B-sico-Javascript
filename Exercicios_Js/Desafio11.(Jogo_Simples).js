de5c6b559ed8df8d42a27dd1c8b245d3af950ad2
Hugox96 <alveshugo2906@gmail.com>
2024-02-14 16:33:44 -0300

    ok

--- Exercicios_Js/Desafio11.(Jogo_Simples).js
+++ Exercicios_Js/Desafio11.(Jogo_Simples).js
@@ -9,34 +9,27 @@ class Personagem {
 
 //Criando os metodos
 
-receberDano(dano) {
-    console.log(`${this.nome} recebeu ${dano} de dano.`);
-    this.vida -= dano;
-    console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
+    receberDano(dano) {
+        console.log(`${this.nome} recebeu ${dano} de dano.`);
+        this.vida -= dano;
+        console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
 
-    if(this.vida <= 30) {
-        // irei criar daqui a pouco
-        this.usarBuster();
+        if(this.vida <= 30) {
+            // irei criar daqui a pouco
+            this.usarBurst();
+        }
+
+        if(this.vida == 0) {
+            console.log("O personagem foi de F");
+        }
+    }    
+
+    usarBurst() {
+        console.log(`${this.nome} usou Burst e recuperou 50 pontos de vida.`);
+        this.vida += 50;
+        console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
+        }
     }
-
-    if(this.vida == 0) {
-        console.log("O personagem foi de F");
-    }
-}    
-
-
-usarBurst() {
-    console.log(`${this.nome} usou Burst e recuperou 50 pontos de vida.`);
-    this.vida += 50;
-    console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
-  }
-
-// usarBurst() {
-//     console.log(`${this.nome} usou Burst e recuperou 50 pontos de vida.`);
-//     this.vida += 50;
-//     console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
-//     }
-}
 
 const jogador1 = new Personagem('Guerreiro', 100);
 const jogador2 = new Personagem('Mago', 80);
